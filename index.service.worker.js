fd33edcad655e2270183c3bf22110f3d874e78c6
tobi-awolaju (kawaii) <120692959+kawaiix621@users.noreply.github.com>
2025-05-02 19:55:47 +0100

    :)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746197641|15550662557';
+const CACHE_VERSION = '1746212136|30046012251';
 /** @type {string} */
 const CACHE_PREFIX = 'Bario-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
